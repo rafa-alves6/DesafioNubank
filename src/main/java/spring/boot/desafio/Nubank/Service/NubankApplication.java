fa8a8847bdab2b30edbb5cfc34b1a2cec5ac2179
Rafael Alves <joao.alves079@academico.ifs.edu.br>
2025-04-18 20:49:10 -0300

    Primeiro commit

---
+++ src/main/java/spring/boot/desafio/Nubank/Service/NubankApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.Nubank.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/desafio/Nubank/NubankApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.Nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
